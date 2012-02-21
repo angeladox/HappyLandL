8669cb339b9e34dfb5e0c5b5dc3be7561f4b1bdb
Brian Kurlychek <brian.kurlychek@maine.edu>
2012-02-21 15:16:59 -0500

    first commit by Angela, Brian and Q. We have tried to do this a few times now..

---
+++ test/integration/com/happyl/LandlordIntegrationTests.groovy
@@ -0,0 +1,24 @@
+package com.happyl
+
+import static org.junit.Assert.*
+import org.junit.*
+import grails.test.*
+
+class LandlordIntegrationTests extends GrailsUnitTestCase{
+    void testSave(){
+        def landlord = new Landlord(userId: 'angelaloo', firstName: 'angela', lastName:'doxsey',email:'angela.doxsey@yahoo.com', password:'heyman',phone:'(207) 730-4455')
+        assertNotNull landlord.save()
+        assertNotNull landlord.userId
+        def foundLandlord = Landlord.get(landlord.id)
+        assertEquals ('angelaloo', foundLandlord.userId)
+    }
+}
+
+/*g userId
+    String firstName
+    String lastName
+    String email
+    String password
+    Date dateCreated
+    String phone*/
+
