b5658c0acd1ef5456718be904c99b3e60de8321e
brianpedalsteel <brianpedalsteel@192.168.1.106>
2012-02-25 12:49:58 -0500

    Added Unit Tests for Unit, Property, and Tenant. -ABQ

---
+++ test/unit/com/happyl/PropertyUnitTests.groovy
@@ -0,0 +1,26 @@
+package com.happyl
+
+import static org.junit.Assert.*
+import grails.test.*
+import org.junit.*
+
+class PropertyUnitTests extends GrailsUnitTestCase{
+    void testConstraints() {
+        def place = new Property(address: "5 sydney way")
+        mockForConstraintsTests(Property,[place]) 
+        def testP = new Property() 
+        assertFalse testP.validate() 
+        assertEquals "nullable",
+        testP.errors["address"] 
+        assertEquals "nullable",
+        testP.errors["state"]
+      
+        testP = new Property(address: "5 sydney way", city: "portland", state: "meine", zipCode: "04102", heating: "oil") 
+        assertFalse testP.validate()      
+        assertTrue(testP.hasErrors())
+               
+        testP = new Property(address: "5 sydney way", city: "portland", state: "Maine", zipCode: "04102", heating: "oil")
+        assertTrue testP.validate()
+        assertFalse(testP.hasErrors())
+    }
+}
