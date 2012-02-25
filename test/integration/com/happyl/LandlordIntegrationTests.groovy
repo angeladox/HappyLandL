110b4780c6f2b705cc659cc0856c619af28be2f4
Brian Kurlychek <brian.kurlychek@maine.edu>
2012-02-25 12:40:27 -0500

    Deleted unused unit tests, fixed unit integration test

--- test/integration/com/happyl/LandlordIntegrationTests.groovy
+++ test/integration/com/happyl/LandlordIntegrationTests.groovy
@@ -6,7 +6,8 @@ import grails.test.*
 
 class LandlordIntegrationTests extends GrailsUnitTestCase{
     void testSave(){
-        def landlord = new Landlord(userId: 'angelaloo', firstName: 'angela', lastName:'doxsey',email:'angela.doxsey@yahoo.com', password:'heyman',phone:'(207) 730-4455')
+        def landlord = new Landlord(userId: 'angelaloo', firstName: 'angela', 
+            lastName:'doxsey',email:'angela.doxsey@yahoo.com', password:'heyman',phone:'(207) 730-4455')
         assertNotNull landlord.save()
         assertNotNull landlord.userId
         def foundLandlord = Landlord.get(landlord.id)
