17cf6f055c988d103ab5c5ab1ad06a6faa536dbd
brianpedalsteel <brianpedalsteel@192.168.1.106>
2012-02-28 19:43:41 -0500

    Email Working!!!! ABQ

--- grails-app/controllers/com/happyl/LandlordController.groovy
+++ grails-app/controllers/com/happyl/LandlordController.groovy
@@ -5,6 +5,7 @@ class LandlordController {
     
     def index= {
         redirect(action: 'register', params: params) }
+ 
     
     def register= {
         if (params){
@@ -12,11 +13,23 @@ class LandlordController {
             if (landlord.validate()) {
                 landlord.save()
                 flash.message = "Successfully Created Landlord" 
+               
+                sendMail {
+                    to params.email /// to where it should go. we should get the email from param
+                    from "Qyaqub@happLandLord.com" ///  not really cool
+                    subject "grails app mail Y!" 
+                    body " Thank you for registering with Happy Landlord! :)"
+                    render "email sent" /// not needed 
+                }
+                
+                
+                
+                
                 redirect(uri: '/property/list', params: params)
             }
-            } else {
-                flash.message = "Error Registering Landlord"
-                return [landlord: landlord]
-            }
+        } else {
+            flash.message = "Error Registering Landlord"
+            return [landlord: landlord]
         }
     }
+}
