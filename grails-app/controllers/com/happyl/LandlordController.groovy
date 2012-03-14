f95bffd046eb1088fcdef5ffe5be32c2ec14a61a
brianpedalsteel <brianpedalsteel@192.168.1.106>
2012-03-14 18:22:12 -0400

    Q and Brian fixed the problem with the messagebundles and the landord controller/register and Q accidentally drank Brian's coffee.

--- grails-app/controllers/com/happyl/LandlordController.groovy
+++ grails-app/controllers/com/happyl/LandlordController.groovy
@@ -4,33 +4,41 @@ class LandlordController {
     def scaffold = true
     
     def index= {
-        redirect(action: 'register', params: params) }
- 
-
+        redirect(uri: '/landlord/register', params:params) }
+    
+    
+    
     def register= {
-        if (params){
-            def landlord = new Landlord(params)
+        if (!params.register) return
+        
+        if (params) {
+            ////////////////////    
+            def landlord = new Landlord(params) 
             if (landlord.validate()) {
+                
                 landlord.save()
-                flash.message = "Successfully Created Landlord" 
-               
+                flash.message = "Successfully Created User"
+                
                 sendMail {
                     to params.email /// to where it should go. we should get the email from param
                     from "Qyaqub@happLandLord.com" ///  not really cool
                     subject "Welcome to happy landlord!" 
                     html '<table> <tr> <td > Dear' + " ${params.firstName}:" + 
                     ' </td></tr><tr><td> <br><p>Thank you for registering with Happy Landlord! :)<br><br><br> The Happy Landlord Team - ABQ - </p> </td></tr></table> '
-                   
+                    
                 }
                 
-                
-                
-                
-                redirect(uri: '/property/list', params: params)
+                redirect(uri: '/') 
+            }else{
+                flash.message = "Error Registering User"
+                [ landlord: landlord ] 
             }
-        } else {
-            flash.message = "Error Registering Landlord"
-            return [landlord: landlord]
-        }
+
+            /////////////////////    
+        }else{
+            [landlord: landlord]
+        }  
     }
 }
+
+
